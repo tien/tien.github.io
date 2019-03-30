c95b8f0b4de2b1788161662af70d4ce651edaaf4
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2019-03-30 23:25:36 +1300

    Have different quote each time

--- assets/js/randomQuote.js
+++ assets/js/randomQuote.js
@@ -13,4 +13,21 @@ const quotesList = [
 
 const mainQuote = document.getElementById("main-quote");
 
-mainQuote.innerText = quotesList[getRandomInt(0, quotesList.length - 1)];
+// Get prev quote from localstorage
+const prevQuoteIndex = parseInt(window.localStorage.getItem("quoteIndex"), 10);
+
+let dice = getRandomInt(0, quotesList.length - 1);
+if (prevQuoteIndex) {
+  // If prev quote have been set in local storage
+  // Re-role dice until we got a different index number
+  while (dice === prevQuoteIndex) {
+    dice = getRandomInt(0, quotesList.length - 1);
+  }
+
+  mainQuote.innerText = quotesList[dice];
+} else {
+  // just set a random quote
+  mainQuote.innerText = quotesList[dice];
+}
+
+window.localStorage.setItem("quoteIndex", dice.toString());
